8696effdd6eb6ef0c1bd7bd3153b4fe2f3d2bab9
mobu <mobu54511@gmail.com>
2026-03-10 21:31:38 +0800

    update post per page

--- src/config.ts
+++ src/config.ts
@@ -6,8 +6,8 @@ export const SITE = {
   title: "Lanxinmob",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
-  postPerIndex: 4,
-  postPerPage: 4,
+  postPerIndex: 7,
+  postPerPage: 7,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
   showArchives: true,
   showBackButton: true, // show back button in post detail
